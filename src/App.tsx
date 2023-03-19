2d18c641a51c221339eca3460a0524c01196ce72
Jin Lee <jl6@ualberta.ca>
2023-03-19 13:21:05 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {ChatEngine} from 'react-chat-engine';
 
-function App() {
+//Using arrow function component
+const App = () => {
+
+  const chatProjectId = process.env.REACT_APP_CHAT_APP_PROJECT_ID;
+  const chatUserSecret = process.env.REACT_APP_CHAT_APP_USER_SECRET;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChatEngine
+      userName='Jin'
+      projectID = {chatProjectId}
+      userSecret = {chatUserSecret}
+    />
+  )
 }
-
-export default App;
+export default App
