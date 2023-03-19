ed0abaaadf704ce47c516f8f50c6266a8089cc5d
Jin Lee <jl6@ualberta.ca>
2023-03-19 14:49:34 -0600

    feat: implemented the chat engine feature and tested it out

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import {ChatEngine} from 'react-chat-engine';
+import DirectMessaging from './DirectMessaging';
 
 //Using arrow function component
 const App = () => {
 
-  const chatProjectId = process.env.REACT_APP_CHAT_APP_PROJECT_ID;
-  const chatUserSecret = process.env.REACT_APP_CHAT_APP_USER_SECRET;
-
   return (
-    <ChatEngine
-      userName='Jin'
-      projectID = {chatProjectId}
-      userSecret = {chatUserSecret}
-    />
+    <DirectMessaging/>
   )
 }
 export default App
